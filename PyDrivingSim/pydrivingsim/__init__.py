6324605df81f64aeeea10fa90c38ce891fcc467e
GiacomoCorradini <corra981@gmail.com>
2023-01-27 16:07:02 +0100

    Add PyDriving simulator

---
+++ PyDrivingSim/pydrivingsim/__init__.py
@@ -0,0 +1,13 @@
+# The world
+from pydrivingsim.world import World
+
+# The Empty Object
+from pydrivingsim.object import VirtualObject
+
+# The Object
+from pydrivingsim.vehicle import Vehicle
+from pydrivingsim.trafficlight import TrafficLight
+from pydrivingsim.obstacle import Obstacle
+
+# The Agent
+from pydrivingsim.agent import Agent
